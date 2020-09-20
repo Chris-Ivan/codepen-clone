51509b41f817236830cb949a93a562e76af62b6e
Christopher Ivan <my.subscription2001@gmail.com>
2020-09-20 14:39:12 +0700

    Updates

---
+++ precache-manifest.7442266302334acd4c2c7ae69bfbcd01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f16b3b654955fb37a3b549bb703299b",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "ac25514d16782ab5c843",
+    "url": "/codepen-clone/static/css/main.f2346ed1.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac25514d16782ab5c843",
+    "url": "/codepen-clone/static/js/main.11e5c510.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
